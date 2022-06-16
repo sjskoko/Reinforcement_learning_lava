858a94e177b80e081ec86d369197fd38b805ad21
Hyunjong Kim <hjkim0811@snu.ac.kr>
2022-06-16 17:59:21 +0900

    .ipynb to .py

--- team4/interaction_lava.py
+++ team4/interaction_lava.py
@@ -12,13 +12,25 @@ def calculate_performance(episodes, env, agent):
         cum_reward = 0.0
 
         while not done:    
-            action = agent.action(s)
+            action = agent.action(s, epi) # 수정
             ns, reward, done, _ = env.step(action)
             cum_reward += reward
             s = ns
         
         episodic_returns.append(cum_reward)
-                    
+
+    # 추가
+    print('max reward:', max(episodic_returns))
+
+    import matplotlib.pyplot as plt
+    x = [i+1 for i in range(len(episodic_returns))]
+    plt.plot(x, episodic_returns)
+    plt.title('Episode Rewards')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.savefig('test_pf.png')
+    plt.close()    
+    
     return np.sum(episodic_returns)
 
 def calculate_sample_efficiency(episodes, env, agent):
@@ -27,30 +39,31 @@ def calculate_sample_efficiency(episodes, env, agent):
     
     for epi in range(episodes):
         
-        s = env.reset()
+        s = env.reset() # s: numpy.int64
 
         done = False
         cum_reward = 0.0
 
         while not done:    
-            action = agent.action(s)
-            ns, reward, done, _ = env.step(action)
+            action = agent.action(s, epi) # 수정
+            ns, reward, done, _ = env.step(action) # ns: np.array
             cum_reward += reward
+            agent.update(ns, action, reward, epi) # 수정
             s = ns
-            
-            #####################
-            # If your agent needs to update the weights at every time step, complete your update process in this area.
-            # e.g., agent.update()
-
-
-            #####################
-        #####################
-        # elif your agent needs to update the weights at the end of every episode, complete your update process in this area.
-        # e.g., agent.update()
-        
-        #####################
         
         episodic_returns.append(cum_reward)
+
+    # 추가
+    print('max reward:', max(episodic_returns))
+
+    import matplotlib.pyplot as plt
+    x = [i+1 for i in range(len(episodic_returns))]
+    plt.plot(x, episodic_returns)
+    plt.title('Episode Rewards')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.savefig('test_se.png')
+    plt.close()        
                     
     return np.sum(episodic_returns)
 
