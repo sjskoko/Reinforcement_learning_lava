5abca7244c50eb935209c3dd1a515215343d3703
sjskoko <77188135+sjskoko@users.noreply.github.com>
2022-06-22 22:07:10 +0900

    220622 최종 제출

--- team4/agent_lava.py
+++ team4/agent_lava.py
@@ -5,14 +5,15 @@ import math
 
 class agent():
     
-    def __init__(self, learning_rate=0.005, gamma=0.95, noise1=50.0, noise2=1.0):
+    def __init__(self, nState=60, nAction=4, learning_rate=0.005, gamma=0.95, noise1=50.0, noise2=1):
         
-        self.nS = 60
-        self.nA = 4
+        self.nS = nState
+        self.nA = nAction
         self.model = torch.nn.Sequential(
             torch.nn.Linear(self.nS,self.nA,bias=False)
         )
 
+        self.learning_rate = learning_rate
         self.gamma = gamma
         self.noise1 = noise1
         self.noise2 = noise2
@@ -24,13 +25,17 @@ class agent():
         self.qval_temp = None
 
     def load_weights(self):
-        self.model.load_state_dict(torch.load('model/DQN_0.005_0.95_50.0_1.0_10000.pt'))
+        noise2_tmp = self.noise2
+        if self.noise2 == None:
+            noise2_tmp = 0
+        # self.model.load_state_dict(torch.load(f'C:/Users/sjsko/PycharmProjects/Reinforcement_learning_lava/2nd_submission_final/models/{self.learning_rate}_{self.gamma}_{self.noise1}_{noise2_tmp}.pt'))
+        self.model.load_state_dict(torch.load(f'final_model.pt'))
 
-    def action(self, state, epi): # state: int 또는 numpy.array
+    def action(self, state):
         self.step += 1
 
-        if type(state) == np.int64:
-            state = np.eye(self.nS)[state] # 벡터로
+        if type(state) == np.int64 or type(state) == np.int32:
+            state = np.eye(self.nS)[state]
         state = torch.from_numpy(state).float()
 
         self.qval_temp = self.model(state)
@@ -39,13 +44,12 @@ class agent():
         # add random noise to actions
         if self.noise1 != None:
             qval_ += np.random.rand(self.nA) * self.noise1/(self.step+1)
-            # qval_ += np.random.rand(self.nA) * self.noise1/(epi+1)
 
         a = np.argmax(qval_)
 
         return a
 
-    def update(self, new_state, action, reward, epi):
+    def update(self, new_state, action, reward):
         new_state = torch.from_numpy(new_state).float() 
 
         with torch.no_grad():
@@ -54,10 +58,9 @@ class agent():
         # add random noise to actions
         if self.noise2 != None:
             newQ += torch.randn(self.nA) * self.noise2/(self.step+1)
-            # newQ += torch.randn(self.nA) * self.noise2/(epi+1) 
 
         maxQ = torch.max(newQ) 
-        if reward == -0.01: # 이 값 바뀌게 되면 어떡함? 
+        if reward == -0.01:
             Y = reward + (self.gamma * maxQ)
         else:
             Y = reward
--- team4/interaction_chainMDP.py
+++ team4/interaction_chainMDP.py
@@ -1,9 +1,10 @@
 import numpy as np
+from tqdm import tqdm
 
 def calculate_performance(episodes, env, agent):
 
     episodic_returns = []
-    max_reward = 0
+    
     for epi in range(episodes):
         
         s = env.reset()
@@ -17,22 +18,17 @@ def calculate_performance(episodes, env, agent):
             cum_reward += reward
             s = ns
         
-        episodic_returns.append(cum_reward)  
-        if cum_reward>=max_reward:
-            max_reward = cum_reward     
-    # print(max_reward)
-    # print(np.sum(episodic_returns))
-    
-    
+        episodic_returns.append(cum_reward)
+                    
     return np.sum(episodic_returns)
 
 def calculate_sample_efficiency(episodes, env, agent):
 
     episodic_returns = []
-    max_reward = 0
-    for epi in range(episodes):
+    
+    for epi in tqdm(range(episodes)):
         
-        s = env.reset() 
+        s = env.reset()
 
         done = False
         cum_reward = 0.0
@@ -41,14 +37,23 @@ def calculate_sample_efficiency(episodes, env, agent):
             action = agent.action(s)
             ns, reward, done, _ = env.step(action)
             cum_reward += reward
-            agent.update(ns, action, reward)
-            s = ns
-        
-        episodic_returns.append(cum_reward)  
-        if cum_reward>=max_reward:
-            max_reward = cum_reward     
-    # print(max_reward)
-    # print(np.sum(episodic_returns))
+            
+            #####################
+            # If your agent needs to update the weights at every time step, complete your update process in this area.
+            agent.observe(s, action, ns, reward, done)
+            agent.update_after_step()
 
+            #####################
+            s = ns
+
+        #####################
+        # elif your agent needs to update the weights at the end of every episode, complete your update process in this area.
+        # e.g., agent.update()
+
+
+        #####################
+        
+        episodic_returns.append(cum_reward)
+                    
     return np.sum(episodic_returns)
 
